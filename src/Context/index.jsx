57e7a4efad667252262bf13521c4fb93f987ef4e
Luis Herrera <luis378962@gmail.com>
2023-06-08 17:30:02 -0600

    input con search agregado

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,57 +1,87 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
-export const ShoppingCartContext = createContext()
+export const ShoppingCartContext = createContext();
 
-export const ShoppingCartProvider = ({children}) => {
-        // Shopping cart Increment quantity
-    const [count, setCount] = useState(0);
+export const ShoppingCartProvider = ({ children }) => {
+  // Shopping cart Increment quantity
+  const [count, setCount] = useState(0);
 
-    //Product detail Open Close
+  //Product detail Open Close
 
-    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false);
-    const openProductDetail = () => setIsProductDetailOpen(true)
-    const closeProductDetail = () => setIsProductDetailOpen(false)
+  const [isProductDetailOpen, setIsProductDetailOpen] = useState(false);
+  const openProductDetail = () => setIsProductDetailOpen(true);
+  const closeProductDetail = () => setIsProductDetailOpen(false);
 
-    //Checkout side menu Open Close
+  //Checkout side menu Open Close
 
-    const [isCheckoutSideMenuOpen, setIsCheckoutSideMenuOpen] = useState(false);
-    const openCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(true)
-    const closeCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(false)
+  const [isCheckoutSideMenuOpen, setIsCheckoutSideMenuOpen] = useState(false);
+  const openCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(true);
+  const closeCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(false);
 
-    // Product detail *** show prodcut
-    const [productToShow, setProductToShow] = useState({
-        title: "",
-        price: "",
-        description: "",
-        images: [],
-    });
-    
-    //Shopping Cart Add procucts to cart
-    const [cartProducts, setCartProducts] = useState([]);
+  // Product detail *** show prodcut
+  const [productToShow, setProductToShow] = useState({
+    title: "",
+    price: "",
+    description: "",
+    images: [],
+  });
 
-    //Shoping Cart Order
-    const [order, setOrder] = useState([]);
+  //Shopping Cart Add procucts to cart
+  const [cartProducts, setCartProducts] = useState([]);
 
+  //Shoping Cart Order
+  const [order, setOrder] = useState([]);
 
-    return (
-        <ShoppingCartContext.Provider value={{
-            count,
-            setCount,
-            openProductDetail,
-            closeProductDetail,
-            isProductDetailOpen,
-            productToShow,
-            setProductToShow,
-            cartProducts,
-            setCartProducts,
-            isCheckoutSideMenuOpen,
-            openCheckoutSideMenu,
-            closeCheckoutSideMenu,
-            order,
-            setOrder
+  // GET PRODUCTS
+  const [items, setItems] = useState(null);
 
-        }}>
-            {children}
-        </ShoppingCartContext.Provider>
-    )
-}
+  const [filteredItems, setFilteredItems] = useState(null);
+
+  // GET PRODUCTS by title
+  const [searchByTitle, setSearchByTitle] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+      .then((response) => response.json())
+      .then((data) => setItems(data));
+  }, []);
+
+  const filteredItemsByTitle = (items, searchByTitle) => {
+    return items?.filter((item) =>
+      item.title.toLowerCase().includes(searchByTitle.toLowerCase())
+    );
+  };
+
+  useEffect(() => {
+    if (searchByTitle)
+      setFilteredItems(filteredItemsByTitle(items, searchByTitle));
+  }, [items, searchByTitle]);
+
+  return (
+    <ShoppingCartContext.Provider
+      value={{
+        count,
+        setCount,
+        openProductDetail,
+        closeProductDetail,
+        isProductDetailOpen,
+        productToShow,
+        setProductToShow,
+        cartProducts,
+        setCartProducts,
+        isCheckoutSideMenuOpen,
+        openCheckoutSideMenu,
+        closeCheckoutSideMenu,
+        order,
+        setOrder,
+        items,
+        setItems,
+        searchByTitle,
+        setSearchByTitle,
+        filteredItems,
+      }}
+    >
+      {children}
+    </ShoppingCartContext.Provider>
+  );
+};
